228a8356f58a7057195ff8efcb1f834d1fe30d2d
Carlos Pavajeau <cantte098@gmail.com>
2022-10-21 16:27:26 -0500

    feat: add api endpoints

---
+++ apps/Electrimax.Api/Endpoints/Orders.cs
@@ -0,0 +1,28 @@
+﻿using Electrimax.Orders.Application.Create;
+using Electrimax.Orders.Application.SearchAll;
+using MediatR;
+
+namespace Electrimax.Api.Endpoints;
+
+public static class Orders
+{
+    public static void MapOrdersEndpoints(this WebApplication app)
+    {
+        app.MapGet("/orders", GetOrders);
+        app.MapPost("/orders", CreateOrder);
+    }
+
+    private static async Task<IResult> GetOrders(IMediator mediator)
+    {
+        var orders = await mediator.Send(new SearchAllOrdersQuery());
+
+        return Results.Ok(orders);
+    }
+
+    private static async Task<IResult> CreateOrder(IMediator mediator, [FromBody] CreateOrderCommand command)
+    {
+        var order = await mediator.Send(command);
+
+        return Results.Ok(order);
+    }
+}
---
+++ apps/Electrimax.Api/Endpoints/SalesDepartments.cs
@@ -0,0 +1,29 @@
+﻿using Electrimax.SalesDepartments.Application.Create;
+using Electrimax.SalesDepartments.Application.SearchAll;
+using MediatR;
+
+namespace Electrimax.Api.Endpoints;
+
+public static class SalesDepartments
+{
+    public static void MapSalesDepartmentEndpoints(this WebApplication app)
+    {
+        app.MapGet("/sales-departments", GetSalesDepartments);
+        app.MapPost("/sales-departments", CreateSalesDepartment);
+    }
+
+    private static async Task<IResult> GetSalesDepartments(IMediator mediator)
+    {
+        var result = await mediator.Send(new SearchAllSalesDepartmentsQuery());
+
+        return Results.Ok(result);
+    }
+
+    private static async Task<IResult> CreateSalesDepartment(IMediator mediator,
+        [FromBody] CreateSalesDepartmentCommand command)
+    {
+        var result = await mediator.Send(command);
+
+        return Results.Ok(result);
+    }
+}
